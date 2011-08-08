9e793105a2190eed57eaf252345e924b71300cbf
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-08-08 11:55:33 +0000

    Import of MANWAR/WWW-Google-Moderator-0.03 from CPAN.
    gitpan-cpan-distribution: WWW-Google-Moderator
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MANWAR/WWW-Google-Moderator-0.03.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/WWW/Google/Moderator.pm
+++ lib/WWW/Google/Moderator.pm
@@ -20,11 +20,11 @@ WWW::Google::Moderator - Interface to Google Moderator API.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 Readonly my $API_VERSION => 'v1';
 Readonly my $BASE_URL    => "https://www.googleapis.com/moderator/$API_VERSION";
 
